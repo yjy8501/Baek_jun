4090f43137cbae45ed0260db83b2440025a45389
Yang, Jaeyong <yjy850146@gmail.com>
2021-02-19 09:16:53 +0900

    백준 1008번 문제

---
+++ src/M_1008/M_1008.java
@@ -0,0 +1,23 @@
+package M_1008;
+
+import java.util.Scanner;
+
+public class M_1008 {
+
+	public static void main(String[] args) {
+
+		double c;
+
+		Scanner sc = new Scanner(System.in);
+
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+
+		System.out.println(a/b);
+
+
+
+
+	}
+
+}
